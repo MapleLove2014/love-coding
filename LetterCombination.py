035bcaed1320f60c631e37fca9334a8a46d4d190
Mr.Zhang <maplelove2014@outlook.com>
2020-07-19 10:26:00 +0800

    backtrack letter combination

--- LetterCombination.py
+++ LetterCombination.py
@@ -1,39 +1,31 @@
 class Solution:
-    def letterCombinations(self, digits: str) -> List[str]:
-        n = len(digits)
-        tree = [0] * (3 ^ n + 1)
-        for i in range(len(digits)):
-            start = (3 ^ (i + 1) - 1) // 2
-            nodes = 3 ^ i
-            a, b, c = self.getDigitChars(digits[i])
-            while start < (3 ^ (i + 2) - 1) // 2:
-                tree[start] = a
-                tree[start + 1] = b
-                tree[start + 2] = c
-                start += 3
-        # 广度优先遍历
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        if not digits:
+            return []
+        result = []
+        self.backtrack(result, '', digits)
+        return result
+        
 
-        result  = []
+    def backtrack(self, result, combination, digits):
+        if not digits:
+            result.append(combination)
+        else:
+            phone = {'2': ['a', 'b', 'c'],
+                 '3': ['d', 'e', 'f'],
+                 '4': ['g', 'h', 'i'],
+                 '5': ['j', 'k', 'l'],
+                 '6': ['m', 'n', 'o'],
+                 '7': ['p', 'q', 'r', 's'],
+                 '8': ['t', 'u', 'v'],
+                 '9': ['w', 'x', 'y', 'z']}
+            for letter in phone[digits[0]]:
+                self.backtrack(result, combination + letter, digits[1:])
 
+s = Solution()
 
-    def travel(self, tree, digits):
-        lastLevel = len(digits) - 1
-        start = self.getLevelStart(lastLevel)
-        memory = [0] * (3 ^ (lastLevel - 1))
-
-        for i in range(start, len(tree))
-            
-
-
-
-
-
-
-    def getLevelStart(self, level):
-        return (3 ^ (level + 1) - 1) // 2
-
-
-
-    def getDigitChars(self, digit):
-        start = (int(digit) - 2) * 3 + ord('a')
-        return (chr(start), chr(start+1), chr(start+2))
+print(s.letterCombinations('23'))
