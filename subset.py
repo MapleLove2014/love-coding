097cc45aa8c3245ea2ea38e91cbb8f4c457b5097
ZhangWang <maplelove2014@outlook.com>
2021-03-30 23:19:46 +0800

    add subset

---
+++ subset.py
@@ -0,0 +1,19 @@
+class Solution:
+    def subsets(self, nums):
+        if not nums or len(nums) == 0:
+            return [[]]
+
+        def doSubset(numsLeft, k, subset, results):
+            if k == 0:
+                results.append(subset)
+                return
+            for i in range(k):
+                doSubset(numsLeft[i+1:], k - i - 1, subset | set(numsLeft[i:i+1]), results)
+        results = []
+        for i in range(1, len(nums) + 1):
+            doSubset(nums, i, set(), results)
+        results.append([])
+        return results
+
+s = Solution()
+print(s.subsets([1,2,3]))
