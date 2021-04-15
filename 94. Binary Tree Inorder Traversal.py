64bf67b20963d59783fd33c164cfdd7c2b7c211f
ZhangWang <maplelove2014@outlook.com>
2021-04-15 22:59:12 +0800

    fix

--- 94. Binary Tree Inorder Traversal.py
+++ 94. Binary Tree Inorder Traversal.py
@@ -5,7 +5,7 @@ class TreeNode:
         self.left = left
         self.right = right
 class Solution:
-    def inorderTraversal(self, root: TreeNode) -> List[int]:
+    def inorderTraversal(self, root: TreeNode):
         
         def traversal(root, result):
             if not root:
