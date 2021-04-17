c550083c9aee0f63755afefd52d57d3d8aab19e9
ZhangWang <maplelove2014@outlook.com>
2021-04-17 12:29:32 +0800

    add binary tree level order

---
+++ 102. Binary Tree Level Order Traversal.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def levelOrder(self, root: TreeNode):
+        if not root:
+            return []
+        result = []
+        queue = [root]
+        node = None
+        while len(queue) > 0:
+            result.append([node.val for node in queue])
+            newQueue = []
+            for node in queue:
+                if node.left:
+                    newQueue.append(node.left)
+                if node.right:
+                    newQueue.append(node.right)
+            queue = newQueue
+
+        return result
+                
+            
