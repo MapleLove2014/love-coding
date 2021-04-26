3b5122916c1e2694a1798ee2bf48a46cb402b7c4
ZhangWang <maplelove2014@outlook.com>
2021-04-26 21:37:39 +0800

    add binary tree postorder

---
+++ 145. Binary Tree Postorder Traversal.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        def post(root, result):
+            if not root:
+                return
+            post(root.left, result)
+            post(root.right, result)
+            result.append(root.val)
+        result = []
+        post(root, result)
+        return result
+
+
