39a4eeb5693221b3686c337104c3662ba5427aaf
ZhangWang <maplelove2014@outlook.com>
2021-05-03 18:18:46 +0800

    add jump game

---
+++ 55. Jump Game.py
@@ -0,0 +1,23 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        
+        # dp[i] -> whether you are able to jump to index i
+        # dp[i] = 
+        dp = [False] * len(nums)
+
+        dp[0] = True
+
+        for i in range(1, len(nums)):
+
+            for j in range(i-1, -1, -1):
+
+                if j + nums[j] >= i and dp[j]:
+                    dp[i] = True
+                    break
+        return dp[-1]
+
+
+
+
+
+
