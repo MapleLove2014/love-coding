534e979a5e35d7ed7388eef209984b22c1a2d1ff
ZhangWang <maplelove2014@outlook.com>
2021-05-13 23:49:38 +0800

    add log2n find peak element

--- 162. Find Peak Element.py
+++ 162. Find Peak Element.py
@@ -13,3 +13,13 @@ class Solution:
                 prev = nums[i]
         return -1
 
+    def log2n(self, nums):
+
+        def find(nums, i, j):
+            if i == j:
+                return i
+            mid = (i + j) // 2
+            if nums[mid] > nums[mid + 1]:
+                return find(nums, i, mid)
+            return find(nums, mid + 1, j)
+        return find(nums, 0, len(nums)-1)
