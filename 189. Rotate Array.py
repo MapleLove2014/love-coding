e5d6019074ca521a161ae6bf7b4c344195a90d49
ZhangWang <maplelove2014@outlook.com>
2021-05-16 15:26:01 +0800

    add rotate array

---
+++ 189. Rotate Array.py
@@ -0,0 +1,32 @@
+class Solution:
+    def rotate(self, nums, k: int):
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        count = 0
+        start = 0
+        while count < len(nums):
+            pre = nums[start]
+            current = start
+            while True:  
+                next = (current + k) % len(nums)
+                temp = nums[next]
+                nums[next] = pre
+                pre = temp
+                count += 1
+                current = next
+                if start == current:
+                    break
+            start += 1
+            
+
+
+
+
+
+s = Solution()
+print(s.rotate([1,2,3,4,5], 9))
+print(s.rotate([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53],82))
+print(s.rotate([1,2,3,4,5,6,7], 1))
+print(s.rotate([1,2,3], 4))
