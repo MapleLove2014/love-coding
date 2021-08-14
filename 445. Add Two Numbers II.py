5976489f8936726a5d5b6dc16be96bf7c74ee37d
张旺 <maplelove2014@outlook.com>
2021-08-14 19:12:06 +0800

    添加链表相加方法1

---
+++ 445. Add Two Numbers II.py
@@ -0,0 +1,48 @@
+# Definition for singly-linked list.
+from typing import List, Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __str__(self):
+        return str(self.val) + '->' + str(self.next)
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        l1 = self.reverse(l1)
+        l2 = self.reverse(l2)
+        carry = 0
+        dummy = ListNode()
+        node = dummy
+        while l1 or l2 or carry > 0:
+            part1 = l1.val if l1 else 0
+            part2 = l2.val if l2 else 0
+            s = part1 + part2 + carry
+            carry = s // 10
+            left = s % 10
+
+            node.next = ListNode(left)
+            node = node.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return self.reverse(dummy.next)
+
+    def reverse(self, list: ListNode) -> ListNode:
+        node = list
+        newHead = None
+        while node:
+            nodeNext = node.next
+            node.next = newHead
+            newHead = node
+            node = nodeNext
+        return newHead
+s = Solution()
+r = s.addTwoNumbers(ListNode(7, ListNode(2, ListNode(4, ListNode(3)))), ListNode(5, ListNode(6, ListNode(4))))
+print(r)
+
+print(s.addTwoNumbers(ListNode(5), ListNode(5)))
