c54e7b999dec7640de33560a164a597268eed9a3
张旺 <maplelove2014@outlook.com>
2021-12-08 20:50:37 +0800

    add length of last world

---
+++ 58. Length of Last Word.py
@@ -0,0 +1,14 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        l = 0
+        end = len(s) - 1
+        while end >= 0:
+            if s[end] != ' ':
+                l += 1
+            elif l > 0:
+                return l
+            end -= 1
+        return l
+s = Solution()
+print(s.lengthOfLastWord('Hello World'))
+print(s.lengthOfLastWord('   fly me   to   the moon  ') == 4)
