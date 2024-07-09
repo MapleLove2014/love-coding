7d188b72f392141d29dabd72115acbacb49fd91e
张旺 <maplelove2014@outlook.com>
2024-07-09 11:21:54 +0800

    divide

--- 29. Divide Two Integers.py
+++ 29. Divide Two Integers.py
@@ -14,3 +14,29 @@ class Solution:
             times += 1
 
         return times if pos else -times
+    
+    def divide2(self, dividend: int, divisor: int) -> int:
+        times = 0
+        if dividend == 0:
+            return 0
+        if dividend == divisor:
+            return 1
+        pos = True if (dividend > 0 and divisor > 0) or (dividend < 0 and divisor < 0) else False
+        minV = (1 << 31) - 1
+        if abs(divisor) == 1:
+            if abs(dividend) >= (1 << 31) and pos:
+                return minV
+            else:
+                return abs(dividend) if pos else -abs(dividend)
+        n=abs(dividend)
+        d=abs(divisor)
+        while n >= d :
+            p=0
+            while n >= (d << (p + 1)):
+                p += 1
+            n -= (d << p)
+            times += (1 << p)
+        if times >= (1 << 31) and pos:
+            return minV
+        return times if pos else -times
+
