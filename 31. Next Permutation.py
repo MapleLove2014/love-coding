84cd34f5863a5ff80cc7fddfae211ff8c2a0d423
张旺 <maplelove2014@outlook.com>
2024-07-09 20:34:41 +0800

    31

--- 31. Next Permutation.py
+++ 31. Next Permutation.py
@@ -27,7 +27,37 @@ class Solution:
             j -= 1
         
 
+    def nextPermutation2(self, nums) -> None:
+        def swap(nums, i, j):
+            temp = nums[i]
+            nums[i] = nums[j]
+            nums[j] = temp
+        b = -1
+        for i in reversed(range(len(nums) - 1)):
+            if nums[i] < nums[i+1]:
+                b = i
+                break
+        c = b
+        if b >= 0:
+            for j in reversed(range(len(nums))):
+                if nums[j] > nums[b]:
+                    c=j
+                    swap(nums, b, c)
+                    break
+        # b+1 <-> len(nums) - 1
+        counts = len(nums) - 1 - (b + 1) + 1
+        for j in range(int(counts/2)):
+            l = b + 1 + j
+            r = len(nums) - 1 - j
+            swap(nums, l, r)
+        print(','.join([str(n) for n in nums]))
+
+
+    
 s = Solution()
 s.nextPermutation([1,3,2])
+s.nextPermutation2([1,2])
+s.nextPermutation2([3,2,1])
+s.nextPermutation2([1,2,3])
 #s.nextPermutation([2,3,1,3,3])
 #s.nextPermutation([3,2,1])
