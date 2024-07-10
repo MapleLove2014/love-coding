184337a675b7ef4f235e6bc894aeccc522fcd006
张旺 <maplelove2014@outlook.com>
2024-07-10 11:13:11 +0800

    33

--- 33. Search in Rotated Sorted Array.py
+++ 33. Search in Rotated Sorted Array.py
@@ -21,7 +21,44 @@ class Solution:
 
         return doSearch(nums, 0, len(nums)-1, target)
 
+    def search2(self, nums, target: int) -> int:
+        
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = int((left + right) / 2)
+            if nums[mid] == target:
+                return mid
+            if nums[left] <= nums[mid]:
+                # left ------ mid is sorted
+                if target < nums[mid] and target >= nums[left]:
+                    right = mid - 1
+                elif target < nums[mid] and target < nums[left]:
+                    left = mid + 1
+                else:
+                    left = mid + 1
+            else:
+                # mid ----- right is sorted
+                if target > nums[mid] and target <= nums[right]:
+                    left = mid + 1
+                elif target > nums[mid] and target > nums[right]:
+                    right = mid - 1
+                else:
+                    right = mid - 1
+
+        return -1
+
+                
+
+
+
+
+
 s = Solution()
+
+print(s.search2([3,5,1], 3))
+print(s.search2([4,5,6,7,0,1,2], 3))
+
 print(s.search([7,8,1,2,3,4,5,6], 2) == 3)
 
 print(s.search([4,5,6,1,2,3], 4) == 0)
