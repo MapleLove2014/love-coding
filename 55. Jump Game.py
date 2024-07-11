f3421685cc0d9801bbf697e6fc4929a318f76df0
张旺 <maplelove2014@outlook.com>
2024-07-11 22:17:29 +0800

    55

--- 55. Jump Game.py
+++ 55. Jump Game.py
@@ -15,6 +15,16 @@ class Solution:
                     dp[i] = True
                     break
         return dp[-1]
+    
+    def canJump2(self, nums: List[int]) -> bool:
+        dp = [False] * len(nums)
+        dp[0] = True
+        for i in range(1, len(nums)):
+            for j in range(i-1, -1, -1):
+                if dp[j] and nums[j] >= i - j:
+                    dp[i] = True
+                    break
+        return dp[-1]
 
 
 
