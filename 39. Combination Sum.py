b45f7172e17d1ffe1a89a3822958548eb61b4acf
张旺 <maplelove2014@outlook.com>
2024-07-11 21:02:02 +0800

    39

--- 39. Combination Sum.py
+++ 39. Combination Sum.py
@@ -10,12 +10,35 @@ class Solution:
             if target >= candidates[index]:
                 lookup(candidates, index, target - candidates[index], list(combine) + [candidates[index]], result)
             lookup(candidates, index + 1, target, list(combine), result)
-        
-        sorted(candidates)
         result = []
-        lookup(candidates, 0, target, [], result)
+        lookup(sorted(candidates), 0, target, [], result)
         return result
 
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+
+        def comb(nums, target):
+            result = []
+            for c in nums:
+                if c > target:
+                    break
+                if c == target:
+                    result.append([c])
+                    break
+                subResult = comb(nums, target - c)
+                if len(subResult) > 0:
+                    for s in subResult:
+                        s.append(c)
+                        result.append(s)
+            return result
+        sorted(candidates)
+        result = comb(candidates, target)
+        sr = [sorted(x) for x in result]
+        d = {}
+        for ss in sr:
+            d["-".join([str(t) for t in ss])] = ss
+        
+        return d.values()
+    
 s = Solution()
 print(s.combinationSum([2,3,6,7], 7))
 print(s.combinationSum([2,3,5], 8))
