0f003622db06da1b22029f96c8e9e0bfd6c6df1b
张旺 <maplelove2014@outlook.com>
2024-07-12 14:20:46 +0800

    48

--- 48. Rotate Image.py
+++ 48. Rotate Image.py
@@ -28,5 +28,27 @@ class Solution:
             rotate(matrix, i+1)
         rotate(matrix, 0)
 
+    def rotate2(self, matrix) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+
+        def swap(matrix, i, j, p, q):
+            t=matrix[i][j]
+            matrix[i][j] = matrix[p][q]
+            matrix[p][q] = t
+        for i in range((len(matrix) + 1) // 2):
+            for j in range(i, len(matrix[i]) -1 - i):
+                x=i
+                y=j
+                for k in range(3):
+                    yy=len(matrix) - 1 - x
+                    x=y
+                    swap(matrix, i, j, x, yy)
+                    y = yy                
+        # 1 列数->行数    len(matrix) -1 - 行数 = 列数
+        # 2 列数->行数    len(matrix) -1 - 行数 = 列数
+        # 3 列数->行数    len(matrix) -1 - 行数 = 列数
+        # 4 列数->行数    len(matrix) -1 - 行数 = 列数
 
                     
