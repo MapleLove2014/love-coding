89e3657f05fa72d04c5c87e0ea7effcb8a3b2690
张旺 <maplelove2014@outlook.com>
2024-07-12 09:45:30 +0800

    45

--- 45. Jump Game II.py
+++ 45. Jump Game II.py
@@ -11,4 +11,13 @@ class Solution:
                     dp[i] = min(dp[i], dp[j] + 1)
 
         return dp[-1]
+    def jump2(self, nums: List[int]) -> bool:
+        dp = [len(nums)] * len(nums)
+        dp[0] = 0
+        for i in range(1, len(nums)):
+            m = dp[i]
+            for j in range(i-1, -1, -1):
+                if dp[j] >= 0 and nums[j] >= i - j:
+                    dp[i] = min(dp[i], dp[j] + 1)
+        return dp[-1]
 
