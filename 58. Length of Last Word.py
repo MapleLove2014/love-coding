373584839dfdc0b03a861bddceec592c0f369b7d
张旺 <maplelove2014@outlook.com>
2024-07-16 09:33:29 +0800

    58

--- 58. Length of Last Word.py
+++ 58. Length of Last Word.py
@@ -9,6 +9,16 @@ class Solution:
                 return l
             end -= 1
         return l
+    def lengthOfLastWord(self, s: str) -> int:
+        
+        l=0
+        for ss in reversed(s):
+            if ss != " ":
+                l += 1
+            elif l > 0:
+                return l
+            
+        return l
 s = Solution()
 print(s.lengthOfLastWord('Hello World'))
 print(s.lengthOfLastWord('   fly me   to   the moon  ') == 4)
