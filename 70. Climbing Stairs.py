886d9faafdda9eb241f90e28055cf453497e3250
张旺 <maplelove2014@outlook.com>
2024-07-17 09:43:22 +0800

    70

--- 70. Climbing Stairs.py
+++ 70. Climbing Stairs.py
@@ -11,5 +11,11 @@ class Solution:
         for i in range(3, n+1):
             f[i] = f[i-1] + f[i-2]
         return f[n]
+    def climbStairs2(self, n: int) -> int:
+        dp = [0] * (n + 2)
+        dp[1] = 1
+        for i in range(2, n+2):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[-1]
 s = Solution()
 print(s.climbStairs())
