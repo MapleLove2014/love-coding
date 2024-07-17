5ff34720160e9c992907416983489117b4f47d90
张旺 <maplelove2014@outlook.com>
2024-07-17 16:00:08 +0800

    75

--- 75. Sort Colors.py
+++ 75. Sort Colors.py
@@ -31,8 +31,36 @@ class Solution:
             else:
                 swap(nums, p, k-1)
                 k -= 1
-                
+    def sortColors2(self, nums) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+        def partition(nums, low, high):
+            
+            mid = low + (high - low) // 2
+            pivot = nums[mid]
+            low -= 1
+            high += 1
+            while True:
+                low += 1
+                while nums[low] < pivot:
+                    low += 1
+                high -= 1
+                while nums[high] > pivot:
+                    high -= 1
+                # 1,2,3,5,8,7,6,5
+                if low >= high:
+                    return high
+                nums[low],nums[high] = nums[high], nums[low]
 
+        def quick(nums, low, high):
+            if low < high:
+                pivotIndex = partition(nums, low, high)
+                quick(nums, low, pivotIndex)
+                quick(nums, pivotIndex + 1, high)
+        quick(nums, 0, len(nums) - 1)
+                
 
 
 
