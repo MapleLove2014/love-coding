784d3bd067083d2c8ec7cfe5c066cc38c04483a1
张旺 <maplelove2014@outlook.com>
2024-07-23 21:17:14 +0800

    114

--- 114. Flatten Binary Tree to Linked List.py
+++ 114. Flatten Binary Tree to Linked List.py
@@ -25,3 +25,25 @@ class Solution:
             root.right = flattenedLeft
             return root
         flat(root)
+    def flatten2(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        stack = []
+        r = []
+        while True:
+            while root:
+                r.append(root)
+                stack.append(root)
+                root = root.left
+            if not stack:
+                break
+            root = stack.pop().right
+        h = TreeNode()
+        x = h
+        for t in r:
+            t.left = None
+            t.right = None
+            x.right = t
+            x = t
+        return h.right
