f9ed2d0ac703d3ac7ba47a1f178f0423ad699aa0
张旺 <maplelove2014@outlook.com>
2024-07-23 17:22:10 +0800

    110

--- 110. Balanced Binary Tree.py
+++ 110. Balanced Binary Tree.py
@@ -15,3 +15,13 @@ class Solution:
             return True
         diff = height(root.left) - height(root.right)
         return abs(diff) <= 1 and self.isBalanced(root.left) and self.isBalanced(root.right)
+    def isBalanced2(self, root) -> bool:
+        def check(root):
+            if not root:
+                return True, 0
+            l,lh = check(root.left)
+            r,rh = check(root.right)
+            if not l or not r or abs(lh-rh) > 1:
+                return False, 0
+            return True, 1 + max(lh, rh)
+        return check(root)[0]
