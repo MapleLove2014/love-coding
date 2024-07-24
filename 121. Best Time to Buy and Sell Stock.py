40ebcf43c6e1aa59e701c0c614b5f84effcfbd59
张旺 <maplelove2014@outlook.com>
2024-07-24 11:03:04 +0800

    121

--- 121. Best Time to Buy and Sell Stock.py
+++ 121. Best Time to Buy and Sell Stock.py
@@ -12,6 +12,14 @@ class Solution:
                 f[i] = min(f[i-1], prices[i-1], prices[i])
             result = max(result, prices[i] - f[i])
         return result
+    def maxProfit2(self, prices) -> int:
+        buy = 2**31
+        p = 0
+        for t in prices:
+            p = max(p, t - buy)
+            buy = min(buy, t)
+        return p    
+
 s = Solution()
 print(s.maxProfit([7,1,5,3,6,4]))
 print(s.maxProfit([6,5,4,3,2,1]))
