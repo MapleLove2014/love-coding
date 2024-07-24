a5c7d08665527963988998c30801b614f58cdd55
张旺 <maplelove2014@outlook.com>
2024-07-24 11:29:05 +0800

    125

--- 125. Valid Palindrome.py
+++ 125. Valid Palindrome.py
@@ -3,5 +3,8 @@ class Solution:
         s = ''.join(ch for ch in s if ch.isalnum()).lower()
         return s == s[::-1]
 
+    def isPalindrome2(self, s: str) -> bool:
+        ss = [t for t in filter(lambda c: (ord(c)>=97 and ord(c) <= 122) or (ord(c)>=48 and ord(c) <= 57), s.lower())]
+        return ss == ss[::-1]
 s = Solution()
 print(s.isPalindrome("abc Ba"))
