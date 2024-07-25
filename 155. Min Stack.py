b8bdb91306b55116f7da4123ff7e2c06107e1f0f
张旺 <maplelove2014@outlook.com>
2024-07-25 21:31:52 +0800

    155

--- 155. Min Stack.py
+++ 155. Min Stack.py
@@ -1,18 +1,25 @@
 class MinStack:
 
     def __init__(self):
-        
+        self.stack = []
+        self.minValeIndex = []
 
     def push(self, val: int) -> None:
-        
+        if not self.minValeIndex or self.stack[self.minValeIndex[-1]] >= val:
+            self.minValeIndex.append(len(self.stack))
+        self.stack.append(val)
 
     def pop(self) -> None:
-        
+        if self.stack[-1] == self.stack[self.minValeIndex[-1]]:
+            self.minValeIndex.pop()
+        return self.stack.pop()
 
     def top(self) -> int:
+        return self.stack[-1]
         
 
     def getMin(self) -> int:
+        return self.stack[self.minValeIndex[-1]]
         
 
 
