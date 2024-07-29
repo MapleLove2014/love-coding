c5f8fdeea6be8c80b1c40a0c1119a9e546b27e91
张旺 <maplelove2014@outlook.com>
2024-07-29 15:56:23 +0800

    162

--- 162. Find Peak Element.py
+++ 162. Find Peak Element.py
@@ -23,3 +23,23 @@ class Solution:
                 return find(nums, i, mid)
             return find(nums, mid + 1, j)
         return find(nums, 0, len(nums)-1)
+
+    def findPeakElement3(self, nums: list[int]) -> int:
+        if not nums or len(nums) == 1:
+            return 0 if nums else -1
+        p = -2**31
+        n = nums + [-2**31]
+        for i in range(len(n)):
+            if n[i] < p:
+                return i - 1
+            p = n[i]
+    def findPeakElement2(self, nums: list[int]) -> int:
+        l = 0
+        h = len(nums) - 1
+        while l < h:
+            mid = l + (h-l)//2
+            if nums[mid] > nums[mid + 1]:
+                h = mid
+            else:
+                l = mid + 1
+        return h
