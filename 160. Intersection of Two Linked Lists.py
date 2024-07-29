9aedc1b1b59e8a2c383832ed0f2dca52316b991c
张旺 <maplelove2014@outlook.com>
2024-07-29 10:50:57 +0800

    160

--- 160. Intersection of Two Linked Lists.py
+++ 160. Intersection of Two Linked Lists.py
@@ -14,4 +14,32 @@ class Solution:
 
         return h1
 
+    def getIntersectionNode2(self, headA: ListNode, headB: ListNode):
+        d1 = ListNode(2**31, headA)
+        d2 = ListNode(-2**31, headB)
+
+        def size(head):
+            l = 0
+            while head:
+                l += 1
+                head = head.next
+        def jump(head, n):
+            while n > 0:
+                n -= 1
+                head = head.next
+            return head
+        l1 = size(d1)
+        l2 = size(d2)
+
+        if l1 >= l2:
+            d1 = jump(d1, l1 - l2)
+        else:
+            d2 = jump(d2, l2 - l1)
+        while d1 and d2:
+            if d1.val == d2.val:
+                return d1
+            d1 = d1.next
+            d2 = d2.next
+        return None
+
 
