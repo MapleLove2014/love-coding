4ef7244cebb98589af32accf58d43c7461375137
张旺 <maplelove2014@outlook.com>
2024-07-30 21:28:58 +0800

    168

--- 168. Excel Sheet Column Title.py
+++ 168. Excel Sheet Column Title.py
@@ -11,6 +11,17 @@ class Solution:
             columnNumber = (columnNumber - 1) // 26
 
         return title
+    def convertToTitle2(self, columnNumber: int) -> str:
+        title = ''
+        while columnNumber > 0:
+            l = columnNumber % 26
+            if l == 0:
+                title = 'Z' + title
+                columnNumber = (columnNumber // 26) - 1
+            else:
+                title = chr(ord('A') + l - 1) + title
+                columnNumber = columnNumber // 26
+        return title
 s = Solution()
 print(s.convertToTitle(26))
 print(s.convertToTitle(52))   # AZ
