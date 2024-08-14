114476b0edc0ea005dde71172c2d42264afe72ac
张旺 <maplelove2014@outlook.com>
2024-08-14 11:11:55 +0800

    189

--- 189. Rotate Array.py
+++ 189. Rotate Array.py
@@ -20,6 +20,23 @@ class Solution:
                     break
             start += 1
             
+    def rotate2(self, nums: list[int], k: int):
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if len(nums) < 2 or k == 0:
+            return 
+        k = k % len(nums)
+        def reverse(nums, i, j):
+            while i < j:
+                nums[i],nums[j] = nums[j],nums[i]
+                i += 1
+                j -= 1
+        
+        reverse(nums, 0, len(nums)-1)
+        reverse(nums, 0, k-1)
+        reverse(nums, k, len(nums)-1)
+            
 
 
 
