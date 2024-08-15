21da80643a459e310bb923711079688ff5fa16af
张旺 <maplelove2014@outlook.com>
2024-08-15 09:53:21 +0800

    198

--- 198. House Robber.py
+++ 198. House Robber.py
@@ -12,6 +12,14 @@ class Solution:
                 values[i] = max(values[i-2] + nums[i], values[i-1])
         return values[-1]
 
+
+    def rob2(self, nums: list[int]) -> int:
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        for i in range(1, len(nums)):
+            dp[i] = max((dp[i-2] if i > 1 else 0) + nums[i], dp[i-1])
+        return dp[-1] 
+
 s = Solution()
 print(s.rob([2,7,9,3,1]))
         
