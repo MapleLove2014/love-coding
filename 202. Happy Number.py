f37c3220791cc43bf99688b06e07bd4168344fd7
张旺 <maplelove2014@outlook.com>
2024-08-17 16:12:05 +0800

    202

--- 202. Happy Number.py
+++ 202. Happy Number.py
@@ -12,3 +12,19 @@ class Solution:
                 return False
             n = s
             l.add(n)
+    def isHappy2(self, n: int) -> bool:
+        s = set()
+        while n not in s:
+            if n == 0:
+                return False
+            if n == 1 or (n % 10 == 0 and n // 10 == 0):
+                return True
+            s.add(n)
+            ds = 0
+            while n > 0:
+                b = n % 10
+                if b > 0:
+                    ds += b ** 2
+                n = n // 10
+            n = ds
+        return False
