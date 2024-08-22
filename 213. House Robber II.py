9bb547380bd3e9945e46c06716f26ff75205c1e7
张旺 <maplelove2014@outlook.com>
2024-08-22 20:08:32 +0800

    213

--- 213. House Robber II.py
+++ 213. House Robber II.py
@@ -16,6 +16,17 @@ class Solution:
             return self.robbing(i + 1, nums, robFirst, value1 if robFirst else max(value1, value2 + nums[i]), value1)
         return self.robbing(i + 1, nums, robFirst, max(value1, value2 + nums[i]), value1)
 
+
+    def rob2(self, nums: list[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        dp = [[0]*(len(nums) + 1) for _ in range(2)]
+        dp[1][1] = nums[0]
+        for i in range(2, len(nums) + 1):
+            dp[0][i] = max(dp[0][i-2] + nums[i-1], dp[0][i-1])
+            dp[1][i] = max(dp[1][i-2] + nums[i-1], dp[1][i-1])
+        return max(dp[0][-1], dp[1][-2])
+
 s = Solution()
 print(s.rob([2, 3, 2]) == 3)
 print(s.rob([1,2,3,1]) == 4)
