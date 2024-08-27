4501be77a448620265a12c7f2a7f3cd15ee16fbc
张旺 <maplelove2014@outlook.com>
2024-08-27 09:58:03 +0800

    219

--- 219. Contains Duplicate II.py
+++ 219. Contains Duplicate II.py
@@ -8,4 +8,16 @@ class Solution:
                     return True
             m[n] = i
         return False  
+    def containsNearbyDuplicate2(self, nums: list[int], k: int) -> bool:
+        # nums[i] == nums[j] and abs(i - j) <= k.
+        m = {}
+        for i in range(len(nums)):
+            if nums[i] not in m:
+                m[nums[i]] = []
+            m[nums[i]].append(i)
+        for idx in m.values():
+            for i in range(1, len(idx)):
+                if idx[i] - idx[i-1] <= k:
+                    return True
+        return False
             
