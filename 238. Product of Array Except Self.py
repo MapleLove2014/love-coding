6d9e0b4818200020ebb79664e919b78973752e13
张旺 <maplelove2014@outlook.com>
2024-08-31 16:27:20 +0800

    238

--- 238. Product of Array Except Self.py
+++ 238. Product of Array Except Self.py
@@ -43,6 +43,35 @@ class Solution:
         return result
 
 
+    def productExceptSelf3(self, nums):
+        
+        n = len(nums)
+        pre = [1] * n
+        suf = [1] * n
+        for i in range(1, n):
+            pre[i] = pre[i-1] * nums[i-1]
+        for i in range(n-2, -1, -1):
+            suf[i] =  suf[i+1] * nums[i+1]
+        ans = [1] * n
+        for i in range(n):
+            ans[i] = pre[i] * suf[i]
+        return ans
+
+    def productExceptSelf4(self, nums):
+        
+        n = len(nums)
+        ans = [1] * n
+        curr = 1
+        for i in range(n):
+            ans[i] *= curr
+            curr *= nums[i]
+        curr = 1
+        for i in range(n-1, -1, -1):
+            ans[i] *= curr
+            curr *= nums[i]
+        return ans
+
+
 
 
 s = Solution()
