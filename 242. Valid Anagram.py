005859bcea650bb1f6c46e70d6d66743bd7bd1dd
张旺 <maplelove2014@outlook.com>
2024-09-01 15:18:47 +0800

    242

--- 242. Valid Anagram.py
+++ 242. Valid Anagram.py
@@ -2,3 +2,7 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
 
         return len(s) == len(t) and sorted(s) == sorted(t)
+    
+
+    def isAnagram2(self, s: str, t: str) -> bool:
+        return len(s) == len(t) and sorted(s) == sorted(t)
